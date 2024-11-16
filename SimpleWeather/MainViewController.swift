80adf9929c8e7aa1e515aacd89dc5ffc076fbd30
niket095 <95niket.putilov@gmail.com>
2024-11-16 12:48:26 +0500

    Initial Commit

---
+++ SimpleWeather/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by User on 14.11.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+
+
+}
+
